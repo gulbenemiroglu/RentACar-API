246ec99c842b289bdb225608b8d81134f320059d
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2023-01-15 08:28:33 +0300

    autofac - validation - aop

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Description).MinimumLength(2);
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.Description).Must(StartWithA).WithMessage("Araçların açıklamaları A harfi ile başlamalı");
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(10).When(c => c.BrandId == 1);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+
+        }
+    }
+}
